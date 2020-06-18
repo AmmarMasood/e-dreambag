f5f78bf4cb839faa2f01dc9bf857ebbe78e04cfa
AmmarMasood <ammar.masood98@gmail.com>
2020-06-18 23:15:51 +0500

    netlify deploy

---
+++ build/precache-manifest.393cea918d05e64553555fb37b106cee.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82ab187ab04a2d8daa082732406be75b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f5aba8a6bc4fb8251038",
+    "url": "/static/css/main.6562a670.chunk.css"
+  },
+  {
+    "revision": "479703e70117cdf76c5e",
+    "url": "/static/js/2.57b7263c.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.57b7263c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f5aba8a6bc4fb8251038",
+    "url": "/static/js/main.6bb2a3b7.chunk.js"
+  },
+  {
+    "revision": "2060b9a7e54a59ab7b07",
+    "url": "/static/js/runtime-main.f3a02c50.js"
+  },
+  {
+    "revision": "ad3bb8ed516abd1d7115e10e81683f69",
+    "url": "/static/media/HD-box.ad3bb8ed.jpg"
+  },
+  {
+    "revision": "91b655f1bcfbc3146b3a65ffb0b137b8",
+    "url": "/static/media/UR-box.91b655f1.jpg"
+  },
+  {
+    "revision": "ef5434a6fe0f687cd0fcb662b1f30a88",
+    "url": "/static/media/boxesship.ef5434a6.gif"
+  },
+  {
+    "revision": "475f7f761a02d90e81a68ea6205c84fc",
+    "url": "/static/media/carrier-box.475f7f76.jpg"
+  },
+  {
+    "revision": "fa5fd0d4c8a7bd790b3a23d5ea605eb3",
+    "url": "/static/media/carrier.fa5fd0d4.png"
+  },
+  {
+    "revision": "7572039d14cd5d08223d7988b0575004",
+    "url": "/static/media/final_box_img.7572039d.png"
+  },
+  {
+    "revision": "7b6b0ad5e736ff9b2eccf47c7f98e222",
+    "url": "/static/media/hanmipost_Box.7b6b0ad5.jpg"
+  },
+  {
+    "revision": "8db297d719c6747a6952630a9cf6781b",
+    "url": "/static/media/normalBox.8db297d7.jpg"
+  },
+  {
+    "revision": "9462b889ff6c044da5a5a520dedec9bf",
+    "url": "/static/media/standard.9462b889.jpg"
+  }
+]);
